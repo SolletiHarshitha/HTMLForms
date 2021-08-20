2d61b5617e980b54aab5e011c66b1f4db233ae38
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-21 03:04:05 +0530

    [Harshitha] Added-> Validating name using regex

--- js/Form.js
+++ js/Form.js
@@ -7,9 +7,3 @@ text.addEventListener('input', function(){
     else 
         textError.textContent = "Name is Incorrect";
 });
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
-    output.textContent = salary.value;
-});
